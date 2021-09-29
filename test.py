ea6d3a8cef1fcff7a9558fc810f17d17321f9f79
Emi Tran <emitran@berkeley.edu>
2021-09-28 17:19:01 -0700

    Tested MDP. Successful for 2 small tests

--- test.py
+++ test.py
@@ -2,17 +2,28 @@ from MDP import MDP
 import unittest 
 
 class MDPTestCase(unittest.TestCase):
-    def test_smallMDP(self):
+    def test_small1(self):
         lst = [['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd']]
-        self.mdp = MDP(lst)
-        self.mdp.run()
-        tp = self.mdp.getTransitionProbs()
+        mdp = MDP(lst)
+        mdp.run()
+        # Get the result Transition Probabilities (dictionary)
+        tp = mdp.getTransitionProbs()
         
         solution = {'a': {'b': 1}, 'b': {'c':1}, 'c' : {'d': 1}}
         self.assertEqual(tp, solution)
 
+    def test_small2(self):
+        seq1 = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd']
+        seq2 = ['a', 'b', 'b', 'a', 'a', 'd', 'd', 'b', 'b', 'c', 'a']
+        mdp = MDP([seq1, seq2])
+        mdp.run()
+        # Get the result Transition Probabilities (dictionary)
+        tp = mdp.getTransitionProbs()
+        
+        solution = {'a': {'b': 2/3, 'd': 1/3}, 'b': {'c': 2/3, 'a': 1/3}, 
+                    'c': {'d': 1/2, 'a': 1/2}, 'd': {'b': 1}}
+        self.assertEqual(tp, solution)
         
     
-
 if __name__ == '__main__':
     unittest.main() 
