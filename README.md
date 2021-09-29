db82bad27e80429c946afe7cd2141d3590b61e60
Emi Tran <emitran@berkeley.edu>
2021-09-28 17:45:44 -0700

    Explicitly print out Input & Output

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MDP
-Markov Decision Process for AVT testing. Calculates the transition probabilities for each state (sum to 1) based on an input list of sequences.
+Markov Decision Process for AVT testing. Calculates the transition probabilities for each state (sum to 1) based on an input list of sequences. 
+
+To run tests:
+``` Python
+python test.py -v
+```
+
+Look at test.py for detailed example of input sequences.
--- test.py
+++ test.py
@@ -4,26 +4,36 @@ import unittest
 class MDPTestCase(unittest.TestCase):
     def test_small1(self):
         lst = [['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd']]
+        self.__printInput(lst)
         mdp = MDP(lst)
         mdp.run()
         # Get the result Transition Probabilities (dictionary)
         tp = mdp.getTransitionProbs()
         
+        self.__printOutput(tp)
         solution = {'a': {'b': 1}, 'b': {'c':1}, 'c' : {'d': 1}}
         self.assertEqual(tp, solution)
 
     def test_small2(self):
         seq1 = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd']
         seq2 = ['a', 'b', 'b', 'a', 'a', 'd', 'd', 'b', 'b', 'c', 'a']
-        mdp = MDP([seq1, seq2])
+        lst = [seq1, seq2]
+        self.__printInput(lst)
+        mdp = MDP(lst)
         mdp.run()
         # Get the result Transition Probabilities (dictionary)
         tp = mdp.getTransitionProbs()
         
+        self.__printOutput(tp)
         solution = {'a': {'b': 2/3, 'd': 1/3}, 'b': {'c': 2/3, 'a': 1/3}, 
                     'c': {'d': 1/2, 'a': 1/2}, 'd': {'b': 1}}
         self.assertEqual(tp, solution)
-        
+    
+    def __printInput(self, lst):
+        print("\n......Input: ", lst)
+    
+    def __printOutput(self, o):
+        print(".....Output:", o)
     
 if __name__ == '__main__':
     unittest.main() 
